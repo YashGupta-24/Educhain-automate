6a3b163a3f433d7e1730178db491900c91779d9d
YashGupta-24 <yashgupta.241103@gmail.com>
2025-09-27 15:37:39 +0530

    onlyAdmin Update

--- frontend/lib/contract.ts
+++ frontend/lib/contract.ts
@@ -1,7 +1,8 @@
 import ContractInfo from '../contracts/EduChainAutomate.json';
 
 // This is now the ONLY place we will define our contract address.
-export const contractAddress = '0x111AF48cfCd093Fc428847f11173F4CbA0d29f7a'; // ‼️ PASTE THE ADDRESS HERE ONE LAST TIME
+// export const contractAddress = '0x111AF48cfCd093Fc428847f11173F4CbA0d29f7a'; // ‼️ PASTE THE ADDRESS HERE ONE LAST TIME
+export const contractAddress = '0x490407abb2074Dc4Ab8863f6Af80eEe8588c141A'; // ‼️ PASTE THE ADDRESS HERE ONE LAST TIME
 
 // This exports the ABI.
 export const contractAbi = ContractInfo;
